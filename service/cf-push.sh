81e7b110f0be41af3d335dd2f09aec633bbb2c59
Jon Schneider <jkschneider@gmail.com>
2017-07-27 21:10:04 -0500

    Pass thru requests to atlas from the Spring application's port

---
+++ service/cf-push.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+cf push jon-atlas-cf -p build/libs/atlas-proxy-0.2.0.jar
--- service/src/main/kotlin/io/pivotal/atlas/AtlasApplication.kt
+++ service/src/main/kotlin/io/pivotal/atlas/AtlasApplication.kt
@@ -4,10 +4,22 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.web.support.SpringBootServletInitializer
 import org.springframework.context.annotation.Bean
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.client.RestTemplate
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
+import javax.annotation.PostConstruct
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+import javax.servlet.ServletRegistration
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer
+import java.net.HttpURLConnection
+import java.net.URI
+import java.net.URL
+
 
 /**
  * @author Jon Schneider
@@ -33,4 +45,27 @@ open class AtlasApplication : SpringBootServletInitializer() {
 
     @Bean
     open fun restTemplate() = RestTemplate()
+}
+
+@Controller
+class ExceptionHandlingController {
+    @RequestMapping("/api/v1/**")
+    fun handle404(req: HttpServletRequest, resp: HttpServletResponse) {
+        val url = URL("http://localhost:7101${req.requestURI}")
+        val connection = url.openConnection() as HttpURLConnection
+        req.headerNames.toList().forEach { header ->
+            connection.setRequestProperty(header, req.getHeader(header))
+        }
+        connection.requestMethod = req.method
+        connection.doOutput = true
+        req.inputStream.copyTo(connection.outputStream)
+        connection.connect()
+
+        val responseCode = connection.responseCode
+        if(responseCode == 200)
+            connection.inputStream.copyTo(resp.outputStream)
+
+        resp.status = responseCode
+        println(req.toString())
+    }
 }
